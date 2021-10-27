5344482132cab3dae88047f6cf151fc123e8fca6
三浦翔 <miurashou@miurashounoMacBook-Air.local>
2021-10-27 17:18:04 +0900

    変数等の使い方”

--- src/js/client.js
+++ src/js/client.js
@@ -3,9 +3,27 @@ import ReactDom from "react-dom";
 
 class Layout extends React.Component {
 	render() {
+		let name = "ひよこ"
+		let number01 = 100
+		let number02 = 150
 		return (
-			<h1>It works!</h1>
+			<div>
+				<h1>It works!</h1>
+				<h2>subtitleh2</h2>
+				{/* 変数と文字列の組み合わせ */}
+				<h1>It,s {name}!</h1>
+				{/* 括弧内で計算 */}
+				<h2>足し算{1+2}</h2>
+				{/* 括弧内で変数通しを計算 */}
+				<h2>kake算{number01 * number02}</h2>
+				{/* 関数の呼び出し*/}
+				<p>total_numberは{this.total_number(3,5)}</p>
+			</div>
 		);
+	}
+	// 関数　renderの外に定義
+	total_number(num01,num02) {
+		return num01 + num02;
 	}
 }
 
