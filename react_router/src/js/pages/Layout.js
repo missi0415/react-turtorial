bc1cba02d5e97f72bcca965b83fb06676833e0da
missi0415 <missi0415@gmail.com>
2021-10-28 17:24:24 +0900

    router parameta

--- react_router/src/js/pages/Layout.js
+++ react_router/src/js/pages/Layout.js
@@ -1,11 +1,9 @@
 import React from "react";
-import { Link,withRouter } from "react-router-dom";
-
+import { Link, withRouter } from "react-router-dom";
 class Layout extends React.Component {
   navigate() {
     console.log(this.props.history);
-    // this.props.history.push("/");
-    this.props.history.replace("/")
+    this.props.history.push("/");
   }
   render() {
     console.log(this.props)
@@ -13,10 +11,10 @@ class Layout extends React.Component {
       <div>
         <h1>KillerNews.net</h1>
         {this.props.children}
-        {/* <Link to="/archives">archives</Link>,
-        <Link to="/settings">settings</Link> */}
+        <Link to="/archives/some-other-articles" class="btn btn-warning">archives (some other articles)</Link>
         <Link to="/archives"><button class="btn btn-danger">archives</button></Link>
-        <Link to="/settings"><button class="btn btn-success">settings</button></Link>
+        <Link to="/settings/main"><button class="btn btn-success">settings(main)</button></Link>
+        <Link to="/settings/extra"><button class="btn btn-success">settings(extra)</button></Link>
         <button class="btn btn-info" onClick={this.navigate.bind(this)}>featured</button>
       </div>
     );
