abbcb9da7b0a36ae52dd1f89572357e050ca7586
missi0415 <missi0415@gmail.com>
2021-10-28 11:44:46 +0900

    stateの管理

--- src/js/components/Layout.js
+++ src/js/components/Layout.js
@@ -3,12 +3,21 @@ import Header from "./Header";
 import Footer from "./Footer";
 
 export default class Layout extends React.Component {
+  constructor() {
+    super();
+    this.state = {name: "miura"};
+  }
   render() {
+    setTimeout(
+      () => { this.setState({name: "Hello"}) ;}
+      , 1000
+    );
     return (
       <div>
+        {this.state.name}
         <Header />
         <Footer />
       </div>
-    );
+    )
   }
 }
